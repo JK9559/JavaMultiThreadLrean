2669f22397f2690938cbee63eaca7dd28e782e82
JK9559 <2272728555@qq.com>
2018-09-02 22:08:54 +0800

    commit

---
+++ src/main/java/com/wkang/jk9559/learn/App.java
@@ -0,0 +1,13 @@
+package com.wkang.jk9559.learn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
