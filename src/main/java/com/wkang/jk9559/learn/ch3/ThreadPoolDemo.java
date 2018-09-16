1033797c43347a661a5163dd2539a912dbc8330b
jk9559 <2272728555@qq.com>
2018-09-16 19:51:16 +0800

    commit ThreadPool

---
+++ src/main/java/com/wkang/jk9559/learn/ch3/ThreadPoolDemo.java
@@ -0,0 +1,27 @@
+package com.wkang.jk9559.learn.ch3;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolDemo {
+    public static class MyTask implements Runnable{
+        @Override
+        public void run() {
+            System.out.println(System.currentTimeMillis() + ":Thread ID:" +
+            Thread.currentThread().getId());
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyTask myTask = new MyTask();
+        ExecutorService es = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++){
+            es.submit(myTask);
+        }
+    }
+}
